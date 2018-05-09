b53b972e22bed2cd8c228596e3af72fbe9d67255
Baymi <310617353@qq.com>
2018-05-09 23:58:02 +0800

    add some images

--- src/app/parts/panel/panel.js
+++ src/app/parts/panel/panel.js
@@ -6,7 +6,7 @@ class Panel extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            data: true
+            data: false
         };
     }
 
@@ -26,19 +26,19 @@ class Panel extends Component {
                 <div className="toolBtn">
                     <Button onClick={this.toogleClick.bind(this)}/>
                 </div>
-                <div className="toolPanel">
+                <div className="toolPanel" style={this.state.data ? {width: 0} : {width: 200}}>
                     <Row gutter={16}>
-                        <Col className="gutter-row" span={6}>
-                            <Button className="gutter-box"></Button>
+                        <Col className="gutter-row gutterRow1" span={6}>
+                            <Button className="gutterBox1" style={this.state.data ? {opacity: 0} : {opacity: 1}}/>
                         </Col>
-                        <Col className="gutter-row" span={6}>
-                            <Button className="gutter-box"></Button>
+                        <Col className="gutter-row gutterRow2" span={6}>
+                            <Button className="gutterBox2" style={this.state.data ? {opacity: 0} : {opacity: 1}}/>
                         </Col>
-                        <Col className="gutter-row" span={6}>
-                            <Button className="gutter-box"></Button>
+                        <Col className="gutter-row gutterRow3" span={6}>
+                            <Button className="gutterBox3" style={this.state.data ? {opacity: 0} : {opacity: 1}}/>
                         </Col>
-                        <Col className="gutter-row" span={6}>
-                            <Button className="gutter-box"></Button>
+                        <Col className="gutter-row gutterRow4" span={6}>
+                            <Button className="gutterBox4" style={this.state.data ? {opacity: 0} : {opacity: 1}}/>
                         </Col>
                     </Row>
                 </div>
