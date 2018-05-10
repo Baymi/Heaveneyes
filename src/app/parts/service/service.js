5b2c26552df9d63f96409a0e6d4a6779c593239a
Baymi <310617353@qq.com>
2018-05-10 22:47:04 +0800

    change build page

--- src/app/parts/service/service.js
+++ src/app/parts/service/service.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './style.css';
-import serviceResource from './serviceData';
+//import serviceResource from './serviceData';
 import {Row, Col} from 'antd';
 
 class Service extends Component {
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import "cesium/Source/Widgets/widgets.css";
 import './index.css';
 import 'antd/dist/antd.css';
-import App from './app/App';
+//import App from './app/App';
 import Service from './app/parts/service/service';
 import registerServiceWorker from './registerServiceWorker';
 import buildModuleUrl from "cesium/Source/Core/buildModuleUrl";
 
 buildModuleUrl.setBaseUrl('./cesium/');
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Service/>, document.getElementById('root'));
 
 registerServiceWorker();
