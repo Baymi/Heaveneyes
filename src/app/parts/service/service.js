0a5c50e95ecf05457ff1464136085a6995c23daf
chenweida <chenweida@datopia.cn>
2018-05-11 17:26:09 +0800

    complete service part

--- src/app/parts/service/service.js
+++ src/app/parts/service/service.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './style.css';
-//import serviceResource from './serviceData';
-import {Row, Col} from 'antd';
+import serviceResource from './serviceData';
+import {Button, Row, Col} from 'antd';
 
 class Service extends Component {
     constructor(props, context) {
@@ -10,49 +10,86 @@ class Service extends Component {
     }
 
     componentDidMount() {
-
     }
 
     render() {
-        // const serviceDom;
-        // for (var i of serviceResource) {
-        //     switch (i.status) {
-        //         case 1:
-        //             serviceDom = <Col></Col>;
-        //             break;
-        //         case 2:
-        //             serviceDom = <Col></Col>;
-        //             break;
-        //         case 3:
-        //             serviceDom = <Col></Col>;
-        //             break;
-        //         case 4:
-        //             serviceDom = <Col></Col>;
-        //             break;
-        //     }
-        // }
+        const data = serviceResource.serviceData;
+        let color;
+        const loop = data=>data.map((item)=> {
+            switch (item.status) {
+                case 1:
+                    color = '#03bceb';
+                    break;
+                case 2:
+                    color = '#38c84c';
+                    break;
+                case 3:
+                    color = '#fa615b';
+                    break;
+                case 4:
+                    color = '#bebebe';
+                    break;
+            }
+            return (
+                <Col span={6}>
+                    <div className="sPanel">
+                        <Row className="pRow" type="flex" justify="space-around" align="middle">
+                            <Col span={7}>
+                                <div className="sRate">
+                                    <span className="spanRate1">使用频率</span>
+                                    <span className="spanRate2">{item.rate}</span>
+                                </div>
+                            </Col>
+                            <Col span={10}>
+                                <div className="sName">{item.name}</div>
+                            </Col>
+                            <Col span={3}>
+                                <div className="sStatus" style={{backgroundColor: color}}></div>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+            );
+        });
         return (
-            <div className="serviceContainer">
-                <Row>
-                    <Col span={6}>
-                        <div className="sPanel">
-                            <Row className="pRow" type="flex" justify="space-around" align="middle">
-                                <Col span={7}>
-                                    <div className="sRate">
-                                        <span>使用频率</span>
-                                        <span className="spanRate">26</span>
-                                    </div>
-                                </Col>
-                                <Col span={10}>
-                                    <div className="sName">服务名称</div>
-                                </Col>
-                                <Col span={3}>
-                                    <div className="circle"></div>
-                                </Col>
-                            </Row>
-                        </div>
-                    </Col>
-                </Row>
+            <div className="c">
+                <div className="availableTitle">
+                    <Row gutter={24} className="availableRow" type="flex" justify="start" align="middle">
+                        <Col>可用状态：</Col>
+                        <Col>
+                            <div className="useCol">
+                                <div className="sStatus" style={{backgroundColor: '#03bceb'}}></div>
+                                <span>在线被使用</span>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="useCol">
+                                <div className="sStatus" style={{backgroundColor: '#38c84c'}}></div>
+                                <span>在线被使用</span>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="useCol">
+                                <div className="sStatus" style={{backgroundColor: '#fa615b'}}></div>
+                                <span>在线被使用</span>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="useCol">
+                                <div className="sStatus" style={{backgroundColor: '#bebebe'}}></div>
+                                <span>在线被使用</span>
+                            </div>
+                        </Col>
+                        <Col>
+                            <Button className="startBtn" type="primary">服务部署</Button>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="serviceContainer">
+                    <Row gutter={16} className="outsideRow">
+                        {loop(data)}
+                    </Row>
+                </div>
             </div>
         )
     }
